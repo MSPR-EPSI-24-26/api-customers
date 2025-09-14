4967df25bb763b4e60134a3c80b1da94d6303388
HugoEschlimann <hugo.eschlimann@laposte.net>
2025-09-14 23:01:20 +0200

    fix: vairables brut

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST || 'db-service-customers',
-      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-      username: process.env.DATABASE_USER || 'user',
-      password: process.env.DATABASE_PASSWORD || 'password',
-      database: process.env.DATABASE_NAME || 'api_customers',
+      host: 'db-service-customers',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'api_customers',
       entities: [Customer],
     }),
     CustomerModule,
